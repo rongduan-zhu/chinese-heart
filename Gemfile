0deefcd9d8faa4fb43d8bc8b748d6f9e5ca1fa08
Rongduan Zhu <rongduan.zhu@gmail.com>
2015-11-07 00:27:45 +0800

    added redis as session store, added figaro for dev and test

--- Gemfile
+++ Gemfile
@@ -2,12 +2,14 @@ source 'https://rubygems.org'
 
 gem 'rails', '4.2.4'
 gem 'pg'
+gem 'redis-rails'
 
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 
 group :development, :test do
   gem 'byebug'
+  gem 'figaro'
 end
 
 group :development do
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_app_session'
+Rails.application.config.session_store :redis_store, servers: {
+  host: ENV['redis_host'],
+  port: ENV['redis_port'],
+  db: 0,
+  namespace: 'session'
+}, expires_in: 4.weeks
